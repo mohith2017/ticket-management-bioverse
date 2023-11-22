67d3daa31009bdf423562007dee981bf8f8e59d8
mohith2017 <mohith.m.venkat@gmail.com>
2023-11-21 23:33:27 -0500

    no vercel.json

--- server.js
+++ server.js
@@ -7,60 +7,61 @@ const dev = process.env.NODE_ENV != "production";
 const handle = app.getRequestHandler();
 const port = process.env.SERVER_PORT || 3000;
 
-const express = require("express");
-const app = express();
+// const express = require('express');
+// const { createServer } = require('http');
+// const { parse } = require('url');
 
-app.get("/", (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-    handle(req, res, parsedUrl);
-}).listen(port, (err) => {
-    if (err) throw err;
-    // Console localhost URL and port.
-    console.log(`❯❯ Ready on http://localhost:${port}`);
-    // Console Network URL ad port.
-    const os = require("os");
-    const networkInterfaces = os.networkInterfaces();
-    const addresses = [];
-    for (const k in networkInterfaces) {
-        for (const k2 in networkInterfaces[k]) {
-            const address = networkInterfaces[k][k2];
-            if (address.family === "IPv4" && !address.internal) {
-                addresses.push(address.address);
+// const app = express();
+
+app.prepare().then(() => {
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;
+        handle(req, res, parsedUrl);
+    }).listen(port, (err) => {
+        if (err) throw err;
+        // Console localhost URL and port.
+        console.log(`❯❯ Ready on http://localhost:${port}`);
+        // Console Network URL ad port.
+        const os = require("os");
+        const networkInterfaces = os.networkInterfaces();
+        const addresses = [];
+        for (const k in networkInterfaces) {
+            for (const k2 in networkInterfaces[k]) {
+                const address = networkInterfaces[k][k2];
+                if (address.family === "IPv4" && !address.internal) {
+                    addresses.push(address.address);
+                }
             }
         }
-    }
-    console.log(`❯❯ Ready on http://${addresses[0]}:${port}`);
+        console.log(`❯❯ Ready on http://${addresses[0]}:${port}`);
+    });
 });
 
-app.listen(process.env.SERVER_PORT || 3000, () => {
-  console.log("Running on port 3000.");
-});
 
-// Export the Express API
-module.exports = app;
+// app.get('/', (req, res) => {
+//     const parsedUrl = parse(req.url, true);
+//     const { pathname, query } = parsedUrl;
+//     handle(req, res, parsedUrl);
+//     res.send(`Received a request at ${pathname} with query parameters: ${JSON.stringify(query)}`);
+// });
 
-// app.prepare().then(() => {
-//     createServer((req, res) => {
-//         const parsedUrl = parse(req.url, true);
-//         const { pathname, query } = parsedUrl;
-//         handle(req, res, parsedUrl);
-//     }).listen(port, (err) => {
-//         if (err) throw err;
-//         // Console localhost URL and port.
-//         console.log(`❯❯ Ready on http://localhost:${port}`);
-//         // Console Network URL ad port.
-//         const os = require("os");
-//         const networkInterfaces = os.networkInterfaces();
-//         const addresses = [];
-//         for (const k in networkInterfaces) {
-//             for (const k2 in networkInterfaces[k]) {
-//                 const address = networkInterfaces[k][k2];
-//                 if (address.family === "IPv4" && !address.internal) {
-//                     addresses.push(address.address);
-//                 }
+// const server = createServer(app);
+
+// server.listen(port, (err) => {
+//     if (err) throw err;
+//     console.log(`❯❯ Ready on http://localhost:${port}`);
+//     // Console Network URL and port.
+//     const os = require('os');
+//     const networkInterfaces = os.networkInterfaces();
+//     const addresses = [];
+//     for (const k in networkInterfaces) {
+//         for (const k2 in networkInterfaces[k]) {
+//             const address = networkInterfaces[k][k2];
+//             if (address.family === 'IPv4' && !address.internal) {
+//                 addresses.push(address.address);
 //             }
 //         }
-//         console.log(`❯❯ Ready on http://${addresses[0]}:${port}`);
-//     });
+//     }
+//     console.log(`❯❯ Ready on http://${addresses[0]}:${port}`);
 // });
