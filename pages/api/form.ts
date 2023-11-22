ed8fb3fe9b0576f6d6deb6f13d5ecf72a176d6fc
mohith2017 <mohith.m.venkat@gmail.com>
2023-11-22 01:15:41 -0500

    Debugging vercel deployment- firestore fetch check 2

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -65,19 +65,19 @@ export default async function ContactApi(
 
       var issueRef_fetch = "0";
       const fetch = await docRef.get();
-      if(!fetch.exists){
-        await docRef.set({
-            name: name,
-            issueRef: 1 });
-         issueRef_fetch = "1";
-      }
-      else{
-        issueRef_fetch = fetch.data().issueRef;
-        console.log(issueRef_fetch)
-        issueRef_fetch = (+issueRef_fetch + 1).toString();
-        console.log(issueRef_fetch)
-        docRef.update({issueRef: issueRef_fetch});
-      }
+      // if(!fetch.exists){
+      //   await docRef.set({
+      //       name: name,
+      //       issueRef: 1 });
+      //    issueRef_fetch = "1";
+      // }
+      // else{
+      //   issueRef_fetch = fetch.data().issueRef;
+      //   console.log(issueRef_fetch)
+      //   issueRef_fetch = (+issueRef_fetch + 1).toString();
+      //   console.log(issueRef_fetch)
+      //   docRef.update({issueRef: issueRef_fetch});
+      // }
 
 
     // //   console.log(issueRef);
